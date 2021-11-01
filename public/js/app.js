48f6b4da7d3bed3825bd833682ba411e567bf1a3
sychova <anastasiya.sychova@gmail.com>
2021-11-02 02:14:03 +0300

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Hello client')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const weatherMessage = document.querySelector('#message')
