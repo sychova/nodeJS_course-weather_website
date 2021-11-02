ae50993e7176f7f5ba3508607b152e57bcb40bde
sychova <anastasiya.sychova@gmail.com>
2021-11-03 00:49:06 +0300

    app startup fix

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
     const location = search.value
     weatherMessage.textContent = 'Loading'
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 weatherMessage.textContent = data.error
