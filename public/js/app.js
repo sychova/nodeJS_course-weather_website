79fb92614df6201eac02b248f046bca1df79ab1a
sychova <anastasiya.sychova@gmail.com>
2021-11-29 02:10:55 +0300

    async await to client weather methods

--- public/js/app.js
+++ public/js/app.js
@@ -4,22 +4,20 @@ const weatherIcon = document.querySelector('#weatherIcon')
 const custLocWeather = document.querySelector('#custLoc')
 const currLocWeather = document.querySelector('#currLoc')
 
-custLocWeather.addEventListener('click', (event) => {
+custLocWeather.addEventListener('click', async (event) => {
     event.preventDefault()
     const location = search.value
     weatherMessage.textContent = 'Loading'
-    fetch(`/weather?address=${location}`).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                weatherMessage.textContent = data.error
-            } else {
-                weatherIcon.style.display = 'block'
-                weatherIcon.alt = `${data.description}`
-                weatherIcon.src = `${data.icon}`
-                weatherMessage.textContent = `You're in ${data.location}. The temperature is ${data.temperature}. And it feels like ${data.feelslike}.`
-            }
-        })
-    })
+    const response = await fetch(`/weather?address=${location}`)
+    const data = await response.json()
+    if (data.error) {
+        weatherMessage.textContent = data.error
+    } else {
+        weatherIcon.style.display = 'block'
+        weatherIcon.alt = `${data.description}`
+        weatherIcon.src = `${data.icon}`
+        weatherMessage.textContent = `You're in ${data.location}. The temperature is ${data.temperature}. And it feels like ${data.feelslike}.`
+    }
 })
 
 currLocWeather.addEventListener('click', (event) => {
@@ -27,18 +25,16 @@ currLocWeather.addEventListener('click', (event) => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
-    navigator.geolocation.getCurrentPosition((position) => {
-        fetch(`/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}`).then((response) => {
-            response.json().then((data) => {
-                if (data.error) {
-                    weatherMessage.textContent = data.error
-                } else {
-                    weatherIcon.style.display = 'block'
-                    weatherIcon.alt = `${data.description}`
-                    weatherIcon.src = `${data.icon}`
-                    weatherMessage.textContent = `You are in ${data.location}. The temperature is ${data.temperature}. And it feels like ${data.feelslike}.`
-                }
-            })
-        })
+    navigator.geolocation.getCurrentPosition(async (position) => {
+        const response = await fetch(`/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
+        const data = await response.json()
+        if (data.error) {
+            weatherMessage.textContent = data.error
+        } else {
+            weatherIcon.style.display = 'block'
+            weatherIcon.alt = `${data.description}`
+            weatherIcon.src = `${data.icon}`
+            weatherMessage.textContent = `You are in ${data.location}. The temperature is ${data.temperature}. And it feels like ${data.feelslike}.`
+        }
     })
 })
