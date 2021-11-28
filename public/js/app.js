5d14e414be8a123b9835cc29dddea76cd21daba4
sychova <anastasiya.sychova@gmail.com>
2021-11-29 02:01:09 +0300

    removed unnecessary comment

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-// const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const weatherMessage = document.querySelector('#message')
 const weatherIcon = document.querySelector('#weatherIcon')
