2b0978fe7954666ed453b16b302440272fb2c157
sychova <anastasiya.sychova@gmail.com>
2021-12-07 23:07:09 +0300

    if condition fix

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request-promise-native')
 const geocode = async (address) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoibWFzaWsiLCJhIjoiY2t2MHpsajFrMWV1cjJ1cXd0d3AwaHc5NiJ9.n6ijCTU5dTlCBmQJZzW33g`
     const response = await request({ url, json: true })
-    if (response.features.length === 0) throw new Error ('No match found')
+    if (!response.features.length) throw new Error ('No match found')
         return {
             latitude: response.features[0].center[1],
             longitude: response.features[0].center[0]
